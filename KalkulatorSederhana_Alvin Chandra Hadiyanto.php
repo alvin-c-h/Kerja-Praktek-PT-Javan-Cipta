ce3635a84afcfc1e50230e545c977867ffcc1ea8
alvin-c-h <82612303+alvin-c-h@users.noreply.github.com>
2021-06-09 02:10:44 +0700

    Add files via upload

---
+++ KalkulatorSederhana_Alvin Chandra Hadiyanto.php
@@ -0,0 +1,48 @@
+<?php
+//Alvin Chandra Hadiyanto
+
+function kalkulatorSederhana($inputHuruf){
+    echo 'kalkulator("'.$inputHuruf.'");//output : ';
+
+    str_replace(" ","",$inputHuruf);
+
+    $calculate=explode("+",$inputHuruf);
+
+    if (count($calculate)==1){
+        $calculate=explode("-",$inputHuruf);
+    }
+    else{
+        $hasil=(int)$calculate[0]+(int)$calculate[1];
+        echo $hasil;
+    }
+
+    if (count($calculate)==1){
+        $calculate=explode("x",$inputHuruf);
+    }
+    else{
+        $hasil=(int)$calculate[0]-(int)$calculate[1];
+        echo $hasil;
+    }
+
+    if (count($calculate)==1){
+        $calculate=explode("/",$inputHuruf);
+        if (count($calculate)>1){
+            if ((int)$calculate[1]==0){
+                echo "tidak bisa dilakukan";
+            }
+            else{
+                $hasil=(int)$calculate[0]/(int)$calculate[1];
+                echo $hasil;
+            }
+        }
+    }
+    else{
+        $hasil=(int)$calculate[0]*(int)$calculate[1];
+        echo $hasil;
+    }
+
+}
+
+kalkulatorSederhana("2 / 0");
+
+?>
