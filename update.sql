aa7120aa9e7cd73e6e7e6d84c229dabf807e43fb
alvin-c-h <82612303+alvin-c-h@users.noreply.github.com>
2021-06-10 08:20:15 +0700

    Add files via upload

---
+++ update.sql
@@ -0,0 +1,8 @@
+show databases;
+use sdm;
+show tables;
+
+UPDATE karyawan SET karyawan.status = 'Menikah' WHERE karyawan.nama = 'Farhan Reza';
+UPDATE karyawan SET karyawan.status = 'Menikah' WHERE karyawan.nama = 'Nadia Aulia';
+UPDATE karyawan SET karyawan.status = 'Menikah' WHERE karyawan.nama = 'Abi Isyawara';
+UPDATE karyawan SET karyawan.status = 'Menikah' WHERE karyawan.nama = 'Dani Setiawan';
