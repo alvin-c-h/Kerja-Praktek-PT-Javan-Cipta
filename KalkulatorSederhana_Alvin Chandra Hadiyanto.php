97b21c1c2f2e9a5fe0d29517958f5332a65828ff
alvin-c-h <82612303+alvin-c-h@users.noreply.github.com>
2021-06-12 10:25:14 +0700

    Update KalkulatorSederhana_Alvin Chandra Hadiyanto.php

--- KalkulatorSederhana_Alvin Chandra Hadiyanto.php
+++ KalkulatorSederhana_Alvin Chandra Hadiyanto.php
@@ -8,36 +8,46 @@ function kalkulatorSederhana($inputHuruf){
 
     $calculate=explode("+",$inputHuruf);
 
-    if (count($calculate)>1){
-        $hasil=(float)$calculate[0]+(float)$calculate[1];
+	if (count($calculate)>1){
+		$hasil=(float)$calculate[0]+(float)$calculate[1];
         echo $hasil;
-    }
-    else{
-        $calculate=explode("-",$inputHuruf);
-        if (count($calculate)>1){
-            $hasil=(float)$calculate[0]-(float)$calculate[1];
-            echo $hasil;
+	}
+	else{
+		$min="-";
+    	for ($i = 1; $i < substr_count($inputHuruf,"-"); $i++) {
+        	$min.="-";  
         }
-        else{
-            $calculate=explode("x",$inputHuruf);
-            if (count($calculate)>1){
-                $hasil=(float)$calculate[0]*(float)$calculate[1];
-                echo $hasil;
-            }
-            else{
-                $calculate=explode("/",$inputHuruf);
-                if (count($calculate)>1){
-                    if ((float)$calculate[1]==0){
-                        echo "tidak bisa dilakukan";
-                    }
-                    else{
-                        $hasil=(float)$calculate[0]/(float)$calculate[1];
-                        echo $hasil;
-                    }
-                }
-            }
-        }
-    }
+		$calculate=explode($min,$inputHuruf);
+		if (count($calculate)>1){
+			if (substr_count($inputHuruf,"-")%2==0){
+				$hasil=(float)$calculate[0]+(float)$calculate[1];
+				echo $hasil;
+			}
+			else{
+				$hasil=(float)$calculate[0]-(float)$calculate[1];
+				echo $hasil;
+			}
+		}
+		else{
+			$calculate=explode("x",$inputHuruf);
+			if (count($calculate)>1){
+				$hasil=(float)$calculate[0]*(float)$calculate[1];
+				echo $hasil;
+			}
+			else{
+				$calculate=explode("/",$inputHuruf);
+				if (count($calculate)>1){
+					if ((float)$calculate[1]==0){
+						echo "tidak bisa dilakukan";
+					}
+					else{
+						$hasil=(float)$calculate[0]/(float)$calculate[1];
+						echo $hasil;
+					}
+				}
+			}
+		}
+	}
 }
 
 kalkulatorSederhana("2 / 0");
